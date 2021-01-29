103a119e6346716e46fb0304d1ffc32dcfd7cac5
Dhathika Karthik <dhathikakarthik@gmail.com>
2021-01-29 22:05:09 +0530

    first commit

---
+++ src/main/java/com/docker/yamifood/YamifoodDockerApplication.java
@@ -0,0 +1,13 @@
+package com.docker.yamifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamifoodDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamifoodDockerApplication.class, args);
+	}
+
+}
