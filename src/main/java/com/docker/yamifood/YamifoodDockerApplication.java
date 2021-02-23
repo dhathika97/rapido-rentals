934e8b760c6a89422279b9972720429d4e35b1de
Dhathika Karthik <dhathikakarthik@gmail.com>
2021-02-23 11:23:00 +0530

    update

--- src/main/java/com/docker/yamifood/YamifoodDockerApplication.java
+++ src/main/java/com/docker/yamifood/YamifoodDockerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YamifoodDockerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring Boot");
 		SpringApplication.run(YamifoodDockerApplication.class, args);
 	}
 
